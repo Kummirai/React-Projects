8ccb7626e39aba0c7f2d00f8bc5a2588c93089e3
Kummirai <ajaxmilton@hotmail.com>
2025-08-21 17:14:37 +0200

    Style main movie cards

--- movie-app/src/components/Home.jsx
+++ movie-app/src/components/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from "./Header.jsx";
+import HomeMovieCard from "./HomeMovieCard.jsx";
 
 const Home = ({navLinks}) => {
     return (
         <div className="container-fluid home d-flex main-img p-0">
-            <div className="col-sm-6 main-bg flex-grow-1 p-5">
+            <div className="col-sm-6 main-bg flex-grow-1 p-5 position-relative">
                 <Header navLinks={navLinks} />
+                <div className="main-movie-card-container row col-11">
+                    <HomeMovieCard />
+                    <HomeMovieCard />
+                    <HomeMovieCard />
+                </div>
             </div>
             <div className="col-sm-6 ">
-
             </div>
         </div>
     );
--- movie-app/src/components/HomeMovieCard.jsx
+++ movie-app/src/components/HomeMovieCard.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const HomeMovieCard = () => {
     return (
-        <div>
-            
+        <div className="main-card col-sm-4">
+            <div className="main-card-image">
+                <img className="img-fluid rounded-1" src="src/assets/images/main-image.webp" alt="main image" />
+            </div>
+            <div className="d-flex flex-column text-white mt-3">
+                <h2 className="mb-0 fw-light">Drama</h2>
+                <h3>Dark Waters</h3>
+            </div>
+            <p className="text-white text-end"><i className="fas fa-star"></i>(4.5)</p>
         </div>
     );
 };
