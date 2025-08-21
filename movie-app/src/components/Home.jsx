38a86913625ba8aefe47aeaa24b5748469021389
Kummirai <ajaxmilton@hotmail.com>
2025-08-21 18:28:04 +0200

    Add box shadow to remove div hard edges

--- movie-app/src/components/Home.jsx
+++ movie-app/src/components/Home.jsx
@@ -4,8 +4,9 @@ import HomeMovieCard from "./HomeMovieCard.jsx";
 
 const Home = ({navLinks, movies}) => {
     return (
-        <div className="container-fluid home d-flex  p-0">
-            <div className="col-sm-6 main-bg flex-grow-1 p-5 d-flex flex-column justify-content-between">
+        <div className="container-fluid home position-relative d-flex p-0">
+            <div className="main-bg position-absolute col-sm-6 z-2"></div>
+            <div className="col-sm-5 flex-grow-1 p-5 d-flex flex-column justify-content-between z-2">
                 <Header navLinks={navLinks} />
                 <div className="home-heading col-sm-9 text-white mt-5">
                     <h2 className="mb-3">Browse Your Favorite <br/>Movies Anywhere</h2>
@@ -18,7 +19,7 @@ const Home = ({navLinks, movies}) => {
                     ))}
                 </div>
             </div>
-            <div className="col-sm-6 main-img overflow-hidden">
+            <div className="col-sm-6 main-img overflow-hidden z-0">
                 <img src="
                 https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p14100_p_v13_ap.jpg" className="img-fluid object-fit-cover" alt=""/>
             </div>
