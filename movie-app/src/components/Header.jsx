96f09477e6afb2b934d19df6d4cef3d0530ea5cd
Kummirai <ajaxmilton@hotmail.com>
2025-08-21 16:32:57 +0200

    Place background image behind the gradient overlay

--- movie-app/src/components/Header.jsx
+++ movie-app/src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = ({navLinks}) => {
 
     return (
         <>
-            <nav className="col-sm-6 d-flex justify-content-between align-items-center">
-                <h1 className="display-6 col-sm-6 fs-4 mb-0">Movie App</h1>
-                <ul className="d-flex align-items-center justify-content-between list-unstyled col-sm-6 mb-0">
+            <nav className="col-sm-10 d-flex justify-content-between align-items-center text-white">
+                <h1 className="display-6 col-sm-5 fs-4 mb-0 ">Movie App</h1>
+                <ul className="d-flex align-items-center justify-content-between list-unstyled col-sm-7 mb-0 text-white">
                     { navLinks.map((link, key) => (
                         <li key={key}>
-                            <a className="text-decoration-none d-block" href="#">{link}</a>
+                            <a className="text-decoration-none text-white-50" href="#">{link}</a>
                         </li>
                     ))}
                 </ul>
--- movie-app/src/components/Home.jsx
+++ movie-app/src/components/Home.jsx
@@ -3,8 +3,13 @@ import Header from "./Header.jsx";
 
 const Home = ({navLinks}) => {
     return (
-        <div className="container-fluid p-5">
-            <Header navLinks={navLinks} />
+        <div className="container-fluid home d-flex main-img p-0">
+            <div className="col-sm-6 main-bg flex-grow-1 p-5">
+                <Header navLinks={navLinks} />
+            </div>
+            <div className="col-sm-6 ">
+
+            </div>
         </div>
     );
 };
