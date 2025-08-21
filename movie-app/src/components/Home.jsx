f2932635f31ae5fe142d7f3fcbbd44d9ab835096
Kummirai <ajaxmilton@hotmail.com>
2025-08-21 17:58:03 +0200

    Create main page heading and add styles

--- movie-app/src/components/Home.jsx
+++ movie-app/src/components/Home.jsx
@@ -4,16 +4,23 @@ import HomeMovieCard from "./HomeMovieCard.jsx";
 
 const Home = ({navLinks, movies}) => {
     return (
-        <div className="container-fluid home d-flex main-img p-0">
-            <div className="col-sm-6 main-bg flex-grow-1 p-5 position-relative">
+        <div className="container-fluid home d-flex  p-0">
+            <div className="col-sm-6 main-bg flex-grow-1 p-5 d-flex flex-column justify-content-between">
                 <Header navLinks={navLinks} />
+                <div className="home-heading col-sm-9 text-white mt-5">
+                    <h2 className="mb-3">Browse Your Favorite <br/>Movies Anywhere</h2>
+                    <p>Your ultimate movie companion, on the go, Your movie night, anytime, anywhere.</p>
+                    <button className="btn btn-danger">Browse Movies</button>
+                </div>
                 <div className="main-movie-card-container row col-11">
                     {movies.map(movie => (
                         <HomeMovieCard movie={movie} />
                     ))}
                 </div>
             </div>
-            <div className="col-sm-6 ">
+            <div className="col-sm-6 main-img overflow-hidden">
+                <img src="
+                https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p14100_p_v13_ap.jpg" className="img-fluid object-fit-cover" alt=""/>
             </div>
         </div>
     );
