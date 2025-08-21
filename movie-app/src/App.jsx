f804b7025498ca3a25ac4e5e6067691e7d4398e4
Kummirai <ajaxmilton@hotmail.com>
2025-08-21 23:03:24 +0200

    Add backup image for home page

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -5,29 +5,6 @@ function App() {
 
 
     const navLinks = ["Home", "Movies", "TV Shows", "Trending"];
-    // const backUpMovies = [
-    //     {
-    //         id: 1,
-    //         title: "Jeepers Creepers 2",
-    //         genre: "Drama",
-    //         rating: 7,
-    //         image: "https://m.media-amazon.com/images/M/MV5BMTIxNDI2NzQyNF5BMl5BanBnXkFtZTYwMTY5MTQ3._V1_FMjpg_UX1000_.jpg"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Universal Soldier",
-    //         genre: "War",
-    //         rating: 8,
-    //         image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLIbZgJx8YG-zEXvWq4iJYKEfCyp8_Eo3dAHbHUETcXOGupnl2qwB_hKPmf1EPVrbfB1zuuA"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Dark Waters",
-    //         genre: "Horror",
-    //         rating: 7.3,
-    //         image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQjtv0X0z9POJgMy-nXd9MRKsm4elcBQhoFumGYxyYpug75oxLN48ExBv4UbNEoKyOK2Kad"
-    //     },
-    // ]
 
     const [movies, setMovies] = useState([]);
     const randomIndex = Math.round(Math.random() * movies.length);
@@ -39,10 +16,9 @@ function App() {
         setMovieIndex(randomIndex);
     }, [movies]);
 
-    console.log(movies);
 
     useEffect(() => {
-        const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+        const url = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
         const options = {
             method: 'GET',
             headers: {
