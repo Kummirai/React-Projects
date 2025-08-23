703322489692780599d9e62e234839e867c710ed
Kummirai <ajaxmilton@hotmail.com>
2025-08-23 16:14:53 +0200

    Add the button hover styles for more tv shows, top rated and movies

--- movie-app/src/components/More.jsx
+++ movie-app/src/components/More.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function More({heading}) {
     return (
-        <div className="container d-flex justify-content-end align-items-center">
-            <a className="text-white-50 text-decoration-none fw-bold" href="#">More {heading} <i className="fas fa-angle-double-right"></i></a>
+        <div className="container more  d-flex justify-content-end align-items-center">
+            <a className="text-white-50 text-decoration-none fw-bold"
+               href="#">More {heading} <i className="fas fa-angle-double-right"></i></a>
         </div>
     );
 }
