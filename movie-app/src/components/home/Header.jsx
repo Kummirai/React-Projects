bbf3fab24eb4a4ed6fcf4ce9ce369cc12ea99b92
Kummirai <ajaxmilton@hotmail.com>
2025-08-26 12:46:03 +0200

    Add more routes to different app pages and links to the header

--- movie-app/src/components/home/Header.jsx
+++ movie-app/src/components/home/Header.jsx
@@ -1,19 +1,17 @@
-import React, {useContext} from 'react';
-import {MoviesContext} from "../../context/Context.jsx";
+import {Link} from 'react-router-dom'
 
 const Header = () => {
 
-    const {navLinks, handleTabSelection, tabs} = useContext(MoviesContext);
+    // const {navLinks, handleTabSelection, tabs} = useContext(MoviesContext);
     return (
         <header>
             <nav className="col-sm-8 d-flex justify-content-between align-items-center text-white">
                 <h1 className="display-6 col-sm-5 fs-4 mb-0 gradient-text ">Movie App</h1>
                 <ul className="d-flex align-items-center justify-content-between list-unstyled col-sm-7 mb-0 text-white">
-                    { navLinks.map((link, key) => (
-                        <li key={key}>
-                            <a onClick={()=>handleTabSelection(link)} className={link !== tabs ? "text-decoration-none fw-normal text-white display-6 font-85 gradient-text" : "text-decoration-none fw-normal text-white display-6  font-85 border-bottom pb-1" } href="#">{link}</a>
-                        </li>
-                    ))}
+                    <Link className="text-decoration-none text-white" to="/">Home</Link>
+                    <Link className="text-decoration-none text-white" to="/movies">Movies</Link>
+                    <Link className="text-decoration-none text-white" to="/tvshows">TV Shows</Link>
+                    <Link className="text-decoration-none text-white" to="/watchlist/">Watch List</Link>
                 </ul>
             </nav>
         </header>
