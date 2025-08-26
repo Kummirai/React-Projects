59634c1ad2847e61ae7ec34e5a732f05666e361b
Kummirai <ajaxmilton@hotmail.com>
2025-08-26 22:24:08 +0200

    Dynamically change wallpapers from movies to tv shows vice verse when tabs change

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx";
 import HomeMovieCard from "../components/HomeMovieCard.jsx";
 import MoviesDetails from "../components/MoviesDetails.jsx";
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {MoviesContext} from "../context/Context.jsx";
 import TVShowsContainer from "../components/TVShowsContainer.jsx";
 import MoviesContainer from "../components/MoviesContainer.jsx";
 import Footer from "../components/Footer.jsx";
+import Hero from "../components/Hero.jsx";
+import Pagination from "../components/Pagination.jsx";
 
 const Home = () => {
 
@@ -14,56 +16,21 @@ const Home = () => {
         upComing,
         bgTheme,
         movieDetailTheme,
-        navLinks,
         isMovieDetails,
-        movie,
-        handleTabSelection,
-        tabs
     } = useContext(MoviesContext);
 
     return (
         <>
-            <div className="container-fluid home  position-relative d-flex p-0"
-                 style={!isMovieDetails ? bgTheme : movieDetailTheme}>
-            {isMovieDetails ?
-                <>
-                    <MoviesDetails
-                        navLinks={navLinks}
-                        movie={movie}
-                        handleTabSelection={handleTabSelection}
-                    />
-                </>
-
-                : (
-                <>
-                    <div className="main-bg home-bg position-absolute col-sm-12 z-3"></div>
-                    <div className="col-sm-8 flex-grow-1 p-5 pb-2 home-grid z-3">
-                        <Header
-                            navLinks={navLinks}
-                            handleTabSelection={handleTabSelection}
-                            tabs={tabs}
-                        />
-                        <div className="home-heading text-white">
-                            <h2 className="display-4 gradient-text fw-bolder">Browse Your Favorite <br/>Movies Anywhere</h2>
-                            <p className="mt-4 mb-5 gradient-text ">Your ultimate movie companion, on the go, Your movie night, anytime, anywhere.</p>
-                            <button className="btn btn-danger gradient-text">Browse Movies</button>
-                        </div>
-                        {/*<div className="container d-flex justify-content-between movie-cards">*/}
-
-                        {/*    {movies.map((movie, index) => (*/}
-                        {/*        index > 2 ? "" :*/}
-                        {/*            <HomeMovieCard key={movie.id} movie={movie}/>*/}
-                        {/*    ))*/}
-                        {/*    }*/}
-                        {/*</div>*/}
-                    </div>
-                </>
-            )}
-
-        </div>
-            <TVShowsContainer heading={navLinks[2]} movies={movies}/>
-            <div className='bg-theme-1'>
-                <MoviesContainer heading={navLinks[1]} movies={upComing}/>
+            <div className="container-fluid gx-0 position-relative" style={isMovieDetails ? movieDetailTheme : bgTheme}>
+                <div className="movie-theme-top position-absolute w-100 z-1 h-100"></div>
+                <div className="hero tv-show z-2">
+                    <Header/>
+                    <Hero/>
+                </div>
+                <div className="z-2 position-relative">
+                    <TVShowsContainer movies={movies} heading="TV Shows"/>
+                    <MoviesContainer movies={upComing} heading="Movies"/>
+                </div>
             </div>
             <Footer/>
         </>
