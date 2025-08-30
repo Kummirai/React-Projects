fc71f40a339863f0281e03e9a9e69cecd83de8ea
Kummirai <ajaxmilton@hotmail.com>
2025-08-30 14:46:07 +0200

    Create and style header section

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,13 +1,10 @@
-
-import './App.css'
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <>
-      <div>
-          <h1>Hello Milton</h1>
-      </div>
+        <Header />
     </>
   )
 }
---
+++ todo-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className={"p-4 bg-primary text-white"}>
+            <h1 className={"text-center"}>Todo App</h1>
+        </header>
+    );
+}
+
+export default Header;
